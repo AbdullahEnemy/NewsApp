3543b0c21a35bd9d69fcf1dffc39bdffd81a36df
AbdullahEnemy <akbuddy8@gmail.com>
2024-08-08 14:41:26 +0500

    Website made more interactive

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Navbar from "./compnents/Navbar";
 import React, { Component } from "react";
 import News from "./compnents/News";
-
 export default class App extends Component {
   render() {
     return (
--- newsapp/src/compnents/News.js
+++ newsapp/src/compnents/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import "./news.css";
 
 export class News extends Component {
   constructor() {
@@ -19,7 +20,7 @@ export class News extends Component {
     const { page } = this.state;
     this.setState({ loading: true });
     try {
-      let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=c880f1af66974d4d91043f8724b45b1e&page=${page}&pageSize=9`;
+      let url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c880f1af66974d4d91043f8724b45b1e&page=${page}&pageSize=9`;
       let response = await fetch(url);
 
       if (!response.ok) {
@@ -27,11 +28,7 @@ export class News extends Component {
       }
 
       let data = await response.json();
-      this.setState({
-        articles: data.articles,
-        loading: false,
-        totalArticles: response.totalArticles,
-      });
+      this.setState({ articles: data.articles, loading: false });
     } catch (error) {
       console.error("Error fetching data: ", error);
       this.setState({ loading: false });
@@ -48,57 +45,57 @@ export class News extends Component {
   };
 
   handleNextClick = () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 10)) {
-    } else {
-      this.setState(
-        (prevState) => ({
-          page: prevState.page + 1,
-        }),
-        this.fetchArticles
-      );
-    }
+    this.setState(
+      (prevState) => ({
+        page: prevState.page + 1,
+      }),
+      this.fetchArticles
+    );
   };
 
   render() {
     const { articles, loading, page } = this.state;
 
     if (loading) {
-      return <div>Loading...</div>; // Show a loading message or spinner
+      return <div>Loading...</div>;
     }
 
     return (
-      <div className="container my-3">
-        <h2>Top Headlines</h2>
-        <div className="row">
-          {articles.map((article, index) => (
-            <div className="col-md-4" key={index}>
-              <NewsItem
-                title={article.title ? article.title.slice(0, 45) : ""}
-                description={
-                  article.description ? article.description.slice(0, 88) : ""
-                }
-                imgurl={article.urlToImage}
-                newsUrl={article.url}
-              />
-            </div>
-          ))}
-        </div>
-        <div className="container d-flex justify-content-center my-3">
-          <button
-            type="button"
-            disabled={page <= 1}
-            className="btn btn-dark mx-2"
-            onClick={this.handlePrevClick}
-          >
-            Prev
-          </button>
-          <button
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-          >
-            Next
-          </button>
+      <div className="gradient-background">
+        {" "}
+        <div className="container my-3">
+          <h2>Top Headlines</h2>
+          <div className="row">
+            {articles.map((article, index) => (
+              <div className="col-md-4" key={index}>
+                <NewsItem
+                  title={article.title ? article.title.slice(0, 45) : ""}
+                  description={
+                    article.description ? article.description.slice(0, 88) : ""
+                  }
+                  imgurl={article.urlToImage}
+                  newsUrl={article.url}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="container d-flex justify-content-center my-3">
+            <button
+              type="button"
+              disabled={page <= 1}
+              className="btn btn-dark mx-2"
+              onClick={this.handlePrevClick}
+            >
+              Prev
+            </button>
+            <button
+              type="button"
+              className="btn btn-dark"
+              onClick={this.handleNextClick}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
     );
